ff66b2084917a6f95466e7a1477bf63a29cac930
Lucarry <lucakramp3901@gmail.com>
2021-04-27 17:42:53 +0200

    Überarbeitung der Oberfläche, erste Button Beispiel Funktion

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,34 @@
 package application;
 
-public class Controller {
-	
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TabPane tabPane_Main;
+
+    @FXML
+    private Button btn_login;
+
+    @FXML
+    private Button btn_createAccount;
+
+    @FXML
+    void createAccountisClicked(ActionEvent event) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btn_createAccount.setOnAction(this::createAccountisClicked);
+    }
+
+    private void createAccountisClicked(javafx.event.ActionEvent actionEvent) {
+        tabPane_Main.getSelectionModel().select(1);
+    }
 }
