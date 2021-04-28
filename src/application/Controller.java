e9b108fd45c93027d3dbd104025d95dd42ea4185
Lucarry <lucakramp3901@gmail.com>
2021-04-28 14:46:24 +0200

    Informations Tab hinzugefügt, Button Anfangs Funktionen

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -17,19 +17,58 @@ public class Controller implements Initializable {
     private Button btn_login;
 
     @FXML
-    private Button btn_createAccount;
+    private Button btn_logout;
 
     @FXML
-    void createAccountisClicked(ActionEvent event) {
-    }
+    private Button btn_cancelCreateAccount;
+
+    @FXML
+    private Button btn_createAccountInTab;
+
+    @FXML
+    private Button btn_createAccount;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btn_createAccount.setOnAction(this::createAccountisClicked);
+        btn_cancelCreateAccount.setOnAction(this::cancelCreateAccountIsClicked);
+        btn_createAccountInTab.setOnAction(this::createAccountInTabIsClicked);
+        btn_logout.setOnAction(this::logoutIsClicked);
+        btn_login.setOnAction(this::loginIsClicked);
     }
 
+    // Account-Erstellungs Button beim Login
     @FXML
     private void createAccountisClicked(javafx.event.ActionEvent actionEvent) {
         tabPane_Main.getSelectionModel().select(1);
+        System.out.println("Account kann nun erstellt werden.");
+    }
+
+    // Abbruch beim Account erstellen
+    @FXML
+    private void cancelCreateAccountIsClicked(javafx.event.ActionEvent actionEvent) {
+        tabPane_Main.getSelectionModel().select(0);
+        System.out.println("Account-Erstellung abgebrochen.");
+    }
+
+    // Account-Erstellung
+    @FXML
+    private void createAccountInTabIsClicked(javafx.event.ActionEvent actionEvent) {
+        tabPane_Main.getSelectionModel().select(0);
+        System.out.println("Account wurde erstellt...");
+    }
+
+    // Login
+    @FXML
+    private void loginIsClicked(javafx.event.ActionEvent actionEvent) {
+        tabPane_Main.getSelectionModel().select(2);
+        System.out.println("Login erfolgreich.");
+    }
+
+    // Logut
+    @FXML
+    private void logoutIsClicked(javafx.event.ActionEvent actionEvent) {
+        tabPane_Main.getSelectionModel().select(0);
+        System.out.println("Logout.");
     }
 }
