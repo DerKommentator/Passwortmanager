23ab4679130822072e41199bce24202e8600b4a7
DerKommentator <Kay120@gmx.de>
2021-05-24 18:02:08 +0200

    added search field; dashboard shows database entries (accounts); adding a new account entry works; renamed Website class to AccountInfo

--- src/model/datenstruktur/AccountInfo.java
+++ src/model/datenstruktur/AccountInfo.java
@@ -1,17 +1,48 @@
 package model.datenstruktur;
 
-public class Website {
+public class AccountInfo {
     private String email;
     private String website;
     private String name;
     private String password;
+    private String username;
+    private long id;
 
-    public Website(String email, String website, String name, String password) {
-
+    public AccountInfo(long id, String email, String website, String name, String password, String username) {
+        this.id = id;
+        this.username = username;
         this.email = email;
         this.website = website;
         this.name = name;
         this.password = password;
+    }
+
+    public AccountInfo(String email, String website, String name, String password, String username) {
+        this.username = username;
+        this.email = email;
+        this.website = website;
+        this.name = name;
+        this.password = password;
+    }
+
+    public AccountInfo() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
